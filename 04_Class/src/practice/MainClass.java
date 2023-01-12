01083b82684108687e14758ce983f8784ebbf4ac
jungbyeongin <quddls6@naver.com>
2023-01-12 12:40:43 +0900

    2023-01-12

---
+++ 04_Class/src/practice/MainClass.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		BankAccount myAcc = new BankAccount("010-1111-1111", 50000);
+		BankAccount yourAcc = new BankAccount("010-9999-9999", 100000);
+		
+		myAcc.transfer(yourAcc, 50_000);
+		
+		myAcc.inquiry();
+		yourAcc.inquiry();
+		}
+
+}
