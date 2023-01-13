15a9959e1f0bdcc2272383a87bed46e24307bea9
jungbyeongin <quddls6@naver.com>
2023-01-13 15:41:13 +0900

    2023-01-13

---
+++ 05_Inheritance/src/ex05_upcasting/Alba.java
@@ -0,0 +1,8 @@
+package ex05_upcasting;
+
+public class Alba extends Student {
+
+	public void work() {
+		System.out.println("일한다.");
+	}
+}
---
+++ 05_Inheritance/src/ex05_upcasting/Student.java
@@ -0,0 +1,8 @@
+package ex05_upcasting;
+
+public class Student extends Person {
+
+	public void Study() {
+		System.out.println("공부한다.");
+	}	
+}
